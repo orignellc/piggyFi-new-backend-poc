91ea3b3a5fd31b4a0393c69bd003ad5db9eb95f1
joseph emmanuel kayode (iemarjay) <ejoseph@wikimedia.org>
2022-06-12 01:26:21 +0100

    add password ecryption to update profile

--- src/modules/user/actions/updateUserProfile.js
+++ src/modules/user/actions/updateUserProfile.js
@@ -2,8 +2,10 @@ import UserRecords from "../logics/UserRecords.js";
 import UserWallet, {
   useDefaultUserWalletFactory,
 } from "../logics/UserWallet.js";
+import Encryptor from "../../../helpers/encrypter.js";
 
 export default async function (user, fields) {
+  fields.password = Encryptor.encrypt(fields.password);
   const updatedUser = await new UserRecords(user).update(fields);
   await useDefaultUserWalletFactory(updatedUser).updateKYC(fields);
 
